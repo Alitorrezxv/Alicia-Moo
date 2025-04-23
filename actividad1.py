ece15a5494e175d947f4c2da2396bf6d06317709
Alitorrezxv <107968738+Alitorrezxv@users.noreply.github.com>
2025-04-23 16:29:41 -0500

    Update actividad1.py

--- actividad1.py
+++ actividad1.py
@@ -3,17 +3,4 @@ Original file is located at
     https://colab.research.google.com/drive/1iojmyNHvXLWAPN5LscPjofljUgYgu85X
 """
 
-nombre = input("¿cual es tu nombre? ")
-edad = input("¿cual es tu edad? ")
-universidad = input("¿de que ciudad eres? ")
-
-print(f"Hola {nombre}, veo que tienes {edad} años y eres de la universidad {universidad}.")
-
-num1 = float(input("introduce un numero: "))
-num2 = float(input("introduce un segundo numero: "))
-
-print(f"suma: {num1 + num2}")
-print(f"resta: {num1 - num2}")
-print(f"multiplicación: {num1 * num2}")
-print(f"división: {num1 / num2}")
-     
+print("HOLAAAA MUNDOOO")
